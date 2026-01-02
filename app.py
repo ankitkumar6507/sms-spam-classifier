5e061997b013c29c842e3d2c81cf863952e86622
ankitkumar6507 <ankitrajoore@gmail.com>
2026-01-03 00:03:40 +0530

    Initial commit: SMS Spam Detection ML app

---
+++ app.py
@@ -0,0 +1,42 @@
+import streamlit as st
+import pickle
+import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+tfidf = pickle.load(open('tfidf.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+    x = []
+    for i in text:
+        if i.isalnum() and i not in stopwords.words('english') and i not in string.punctuation:
+            x.append(ps.stem(i))
+    return " ".join(x)
+
+st.title("📩 EMAIL/SMS Spam Classifier")
+st.write("This model predicts whether an SMS message is **Spam** or **Not Spam**.")
+input_sms = st.text_area("Enter the message")
+
+if st.button("Predict"):
+    if input_sms.strip() == "":
+        st.warning("Please enter a message.")
+    else:
+        # Preprocess
+        transformed_sms = transform_text(input_sms)
+
+        # Vectorize
+        vector_input = tfidf.transform([transformed_sms])
+
+        # Predict
+        result = model.predict(vector_input)[0]
+
+        # Output
+        if result == 1:
+            st.error("🚨 Spam Message")
+        else:
+            st.success("✅ Not Spam")
