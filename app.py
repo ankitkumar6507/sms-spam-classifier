a21b9650fe8dfaee96615299ffe1df35b0fd1426
ankitkumar6507 <ankitrajoore@gmail.com>
2026-01-03 11:54:25 +0530

    Add nltk downloads for deployment

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import string
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
+
 
 ps = PorterStemmer()
 tfidf = pickle.load(open('tfidf.pkl', 'rb'))
